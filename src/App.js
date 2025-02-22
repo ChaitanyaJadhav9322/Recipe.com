f192f04ed214bef8aeb15d663d13e6ea999f9dbd
ChaitanyaJadhav9322 <cj551634@gmail.com>
2025-02-22 17:03:35 +0530

    Added project files and updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+ 
+ 
+ 
+
+import "./styles.css";
+import {Explore} from "./components/Explore";
+import {Feed} from "./components/Feed";
+import { AddRecipe } from "./components/AddRecipe";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Chatbot from "./components/Chatbot";
+import Footer from "./components/Footer";
 
 function App() {
+
+    const [recipes, setRecipes] = useState([
+        { image: "https://via.placeholder.com/150", title: "Sample Recipe", desc: "Description here", ingredients: "Sample ingredients" }
+      ]);
+    
+      const addNewRecipe = (recipe) => {
+        setRecipes([...recipes, recipe]);
+      };
+      
+      const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+      useEffect(() => {
+        // Check if user is logged in
+        const user = localStorage.getItem("username");
+        setIsLoggedIn(!!user);
+      }, []);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar isLoggedIn={isLoggedIn} />
+      <div className="content">
+        <Routes>
+          <Route path="/explore" element={<Explore/> } />
+          <Route path="/signin" element={<Login setIsLoggedIn={setIsLoggedIn} />}/> 
+          <Route path="/account" element={<Register setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/chatbot" element={<Chatbot/>} />
+          <Route path="/feed" element={<Feed/>} />
+          <Route path="/add-recipe" element={<AddRecipe addNewRecipe={addNewRecipe} />} />
+        </Routes>
+      </div>
+      <Footer/>
+      
+    </Router>
   );
 }
 
